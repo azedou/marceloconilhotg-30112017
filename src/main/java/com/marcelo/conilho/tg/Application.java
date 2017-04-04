9465aacb89a763fedee69094c350b2ce3367a32c
marceloconilho <marcelo.conilho@hotmail.com>
2017-04-03 21:47:55 -0300

    AGORA VAI, PORRA

--- src/main/java/com/marcelo/conilho/tg/Application.java
+++ src/main/java/com/marcelo/conilho/tg/Application.java
@@ -1,19 +1,12 @@
 package com.marcelo.conilho.tg;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import static com.google.common.io.Resources.getResource;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
@@ -21,17 +14,12 @@ import static com.google.common.io.Resources.getResource;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws FileNotFoundException {
-
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				.setServiceAccount(new FileInputStream(getResource("MarceloConilhoTG-9d7694846529.json").getPath()))
-				.setDatabaseUrl("https://marceloconilhotg.firebaseio.com/")
-				.build();
-		FirebaseApp.initializeApp(options);
 	}
-	 @Override
-    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 }
--- src/main/java/com/marcelo/conilho/tg/controller/IndexController.java
+++ src/main/java/com/marcelo/conilho/tg/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.marcelo.conilho.tg.controller;
 
-import com.marcelo.conilho.tg.model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Hashtable;
 
 /**
  * Created by marce on 30/03/2017.
