236ee89abeafcf084a7268b9d9efd1d96e692be5
mobalho <marcelo.conilho@hotmail.com>
2017-04-06 16:47:59 +0000

    adding correct import

--- src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
+++ src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
@@ -21,7 +21,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
-@PropertySource("classpath:/application.properties}")
+@PropertySource("classpath:resources/application.properties}")
 
 @Configuration
 @EnableMongoRepositories(basePackages = { "com.marcelo.conilho.tg.repository" })
