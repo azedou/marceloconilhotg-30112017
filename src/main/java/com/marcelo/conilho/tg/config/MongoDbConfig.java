35be3293244b5c90325817f3d74d81caf376a89c
mobalho <marcelo.conilho@hotmail.com>
2017-04-06 13:33:30 +0000

    aditing contructor

--- src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
+++ src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
@@ -44,13 +44,8 @@ public class MongoDbConfig extends AbstractMongoConfiguration {
 
          @Override
          public Mongo mongo() throws Exception {
-                 return client;
-         }
-         
-         @Bean
-         public MongoClient getClient() throws UnknownHostException {
                  return new MongoClient(Collections.singletonList(new ServerAddress(System.getenv("OPENSHIFT_MONGODB_DB_HOST"), System.getenv("OPENSHIFT_MONGODB_DB_PORT"))),
-                 Collections.singletonList(MongoCredential.createCredential(System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"), "mongodbtg", System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD").toCharArray())));
+                 Collections.singletonList(MongoCredential.createCredential(System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"), "mongodbtg", System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD").toCharArray())));;
          }
 
          @Bean
