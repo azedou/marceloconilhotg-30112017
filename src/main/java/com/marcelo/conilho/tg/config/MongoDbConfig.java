a251a0c1311bc8224c871dbadea16668997d70a1
mobalho <marcelo.conilho@hotmail.com>
2017-04-06 16:01:31 +0000

    getting from properties

--- src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
+++ src/main/java/com/marcelo/conilho/tg/config/MongoDbConfig.java
@@ -24,18 +24,31 @@ import com.mongodb.ServerAddress;
 @EnableMongoRepositories(basePackages = { "com.marcelo.conilho.tg.repository" })
 public class MongoDbConfig extends AbstractMongoConfiguration {
 
+         @Value("${spring.data.mongodb.host}")
+         protected String host;
+         @Value("${spring.data.mongodb.user}")
+         protected String user;
+         @Value("${spring.data.mongodb.password}")
+         protected String password;
+         @Value("${spring.data.mongodb.database}")
+         protected String database;
+
+         @Value("${spring.data.mongodb.port}")
+         protected int port;
+
+
          @Autowired
          MongoClient client;
 
          @Override
          public String getDatabaseName() {
-                 return "mongodbtg";
+                 return database;
          }
 
          @Bean
          public MongoClient mongoClient() throws Exception {
-                 return new MongoClient(Collections.singletonList(new ServerAddress(System.getenv("OPENSHIFT_MONGODB_DB_HOST"), Integer.valueOf(System.getenv("OPENSHIFT_MONGODB_DB_PORT")))),
-                 Collections.singletonList(MongoCredential.createCredential(System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"), "mongodbtg", System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD").toCharArray())));
+                 return new MongoClient(Collections.singletonList(new ServerAddress(host, port)),
+                 Collections.singletonList(MongoCredential.createCredential(user,database, password.toCharArray())));
          }
          
          @Override
