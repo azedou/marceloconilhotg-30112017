2de21c1b292fd6eefa36b9fa0ed884ace1b359a9
mobalho <marcelo.conilho@hotmail.com>
2017-04-06 13:39:55 +0000

    aditing imports

--- src/main/java/com/marcelo/conilho/tg/Application.java
+++ src/main/java/com/marcelo/conilho/tg/Application.java
@@ -2,10 +2,15 @@ package com.marcelo.conilho.tg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages={"com.marcelo.conilho.tg.config"})
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
