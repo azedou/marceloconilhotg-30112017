0a0b0b6d0d5ea1d7ab700b4c6a1f7b4c2f36fcc4
marceloconilho <marcelo.conilho@hotmail.com>
2017-05-18 08:27:38 -0300

    Commit do desespero

--- src/main/java/com/marcelo/conilho/tg/controller/StudentController.java
+++ src/main/java/com/marcelo/conilho/tg/controller/StudentController.java
@@ -1,12 +1,13 @@
 package com.marcelo.conilho.tg.controller;
 
-import com.marcelo.conilho.tg.model.Course;
 import com.marcelo.conilho.tg.model.Student;
 import com.marcelo.conilho.tg.repository.StudentRepository;
+import com.marcelo.conilho.tg.service.GenerateAllStudentsDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -18,8 +19,9 @@ public class StudentController {
     @Autowired
     StudentRepository studentRepository;
 
+    GenerateAllStudentsDocument generateAllStudentsDocument = new GenerateAllStudentsDocument();
+
     @RequestMapping(value="/allStudents", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
@@ -34,5 +36,11 @@ public class StudentController {
     @RequestMapping("/getStudent")
     public Student getStudant(@RequestParam("ra") String ra){
         return studentRepository.findOne(ra);
+    }
+
+    @RequestMapping("/editDOCX")
+    public String getStudant() throws Exception {
+        generateAllStudentsDocument.replaceName("JOÃO DA SILVA");
+            return "deu bom ";
     }
 }
---
+++ src/main/java/com/marcelo/conilho/tg/service/GenerateAllStudentsDocument.java
@@ -0,0 +1,49 @@
+package com.marcelo.conilho.tg.service;
+
+import org.apache.poi.xwpf.converter.pdf.PdfConverter;
+import org.apache.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by marce on 21/04/2017.
+ */
+
+public class GenerateAllStudentsDocument {
+
+    public void replaceName(String name) throws Exception {
+        XWPFDocument doc = new XWPFDocument(OPCPackage.open("C:\\Users\\marce\\Desktop\\TG2\\tg2repo\\src\\main\\resources\\Apresentacao-Alunos-TG-I.docx"));
+        for (XWPFParagraph p : doc.getParagraphs()) {
+            List<XWPFRun> runs = p.getRuns();
+            if (runs != null) {
+                for (XWPFRun r : runs) {
+                    String text = r.getText(0);
+                    if (text != null && text.contains("[NOME_ALUNO]")) {
+                        text = text.replace("[NOME_ALUNO]", name);
+                        r.setText(text, 0);
+                    }
+                }
+            }
+        }
+        File docx = new File("C:\\Users\\marce\\Desktop\\TG2\\tg2repo\\src\\main\\resources\\testMenor.docx");
+        saveAsPDF(docx);
+
+
+    }
+
+    public void saveAsPDF(File docx) throws IOException, InvalidFormatException {
+        File pdf = new File("C:\\Users\\marce\\Desktop\\TG2\\tg2repo\\src\\main\\resources\\output2.pdf");
+        XWPFDocument docToConvert = new XWPFDocument(OPCPackage.openOrCreate(docx));
+        FileOutputStream fos = new FileOutputStream(pdf);
+        PdfConverter.getInstance().convert(docToConvert, fos , PdfOptions.create() );
+
+    }
+}
