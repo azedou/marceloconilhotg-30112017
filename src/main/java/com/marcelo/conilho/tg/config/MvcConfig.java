450cfb11e7c146ba23e873d33fee0868c700f5b9
mobalho <marcelo.conilho@hotmail.com>
2017-08-09 18:33:22 +0000

    Integrating React

--- src/main/java/com/marcelo/conilho/tg/config/MvcConfig.java
+++ src/main/java/com/marcelo/conilho/tg/config/MvcConfig.java
@@ -29,35 +29,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver
                 = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/");
         viewResolver.setSuffix(".html");
         return viewResolver;
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-
+    
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
-    }
-
-    @Bean
-    public InternalResourceViewResolver jspViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/WEB-INF/views/");
-        bean.setSuffix(".jsp");
-        return bean;
-    }
-
-    @Bean(name = "messageSource")
-    public ReloadableResourceBundleMessageSource getMessageSource() {
-        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
-        resource.setBasename("classpath:messages");
-        resource.setDefaultEncoding("UTF-8");
-        return resource;
     }
 
 }
--- src/main/java/com/marcelo/conilho/tg/controller/IndexController.java
+++ src/main/java/com/marcelo/conilho/tg/controller/IndexController.java
@@ -1,11 +1,8 @@
 package com.marcelo.conilho.tg.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by marce on 30/03/2017.
@@ -13,6 +10,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
-
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 
 }
