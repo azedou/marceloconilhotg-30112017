f9e0402a93e6b51271082f0ba6f036593b98f54a
marceloconilho <marcelo.conilho@hotmail.com>
2017-08-15 15:17:24 -0300

    #Dev-16 - Integrate ReactJS with SpringBoot and Webpack

---
+++ backend/src/main/java/com/marcelo/conilho/tg/config/App.java
@@ -0,0 +1,36 @@
+package com.marcelo.conilho.tg.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.marcelo.conilho.tg" })
+@Configuration
+@EnableAutoConfiguration
+public class App implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    @Value("${spring.profiles.active}")
+    protected String springProfilesActive;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        LOG.info("=======================================");
+        LOG.info("App running with active profiles: {}", springProfilesActive);
+        LOG.info("=======================================");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+}
