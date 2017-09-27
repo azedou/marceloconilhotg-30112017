a3c50abd407350f40fc5cb6bb8db5942086578da
Marcelo Conilho <marcelo.conilho@hotmail.com>
2017-09-27 13:55:54 +0000

    adding collapse for students

---
+++ frontend/src/components/students/StudentsConstants.js
@@ -0,0 +1,4 @@
+export const ALL_STUDENTS_ENDPOINT = "/allStudents";
+export const ADD_STUDENTS_ENDPOINT = "/addStudent";
+export const ALL_STUDENTS_STRING = "allStudents";
+export const ADD_STUDENTS_STRING = "addStudent";
--- frontend/src/components/students/StudentsPage.js
+++ frontend/src/components/students/StudentsPage.js
@@ -3,22 +3,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as studentsActions from '../../actions/studentsActions';
 import StudentsList from './StudentsList';
+import {ALL_STUDENTS_ENDPOINT,
+        ADD_STUDENTS_ENDPOINT,
+        ALL_STUDENTS_STRING,
+        ADD_STUDENTS_STRING} from './StudentsConstants'
+import {Collapse} from 'react-collapse';
 
 class Student extends React.Component{
   constructor(props){
     super(props);
+    this.state = {isAllStudentsOpened: false,
+                  isAddStudentsOpened: false};
+    this.handleClick = this.handleClick.bind(this);
   }
-
   componentWillMount(){
       this.props.fetchStudents();
   }
+  handleClick(e, target) {
+    e.preventDefault();
+    switch(target) {
+    case ALL_STUDENTS_STRING:
+        this.setState({isAllStudentsOpened: !this.state.isAllStudentsOpened});
+        break;
+    case ADD_STUDENTS_STRING:
+        this.setState({isAddStudentsOpened: !this.state.isAddStudentsOpened});
+        break;
+    }
+  }
 
   render(){
+    let isAllStudentsOpened = this.state.isAllStudentsOpened;
+    let isAddStudentsOpened = this.state.isAddStudentsOpened;
     return(
       <div>
         <h3>Students</h3>
           <div>
-            <StudentsList students={this.props.students}/>
+            <a id={ALL_STUDENTS_STRING} onClick={(e) => this.handleClick(e, ALL_STUDENTS_STRING)}><h4>{ALL_STUDENTS_ENDPOINT}</h4></a>
+            <Collapse isOpened={isAllStudentsOpened} >
+              <StudentsList students={this.props.students}/>
+            </Collapse>
+            <a id={ADD_STUDENTS_STRING} onClick={(e) => this.handleClick(e,ADD_STUDENTS_STRING)}><h4>{ADD_STUDENTS_ENDPOINT}</h4></a>
+            <Collapse isOpened={isAddStudentsOpened} >
+              <StudentsList students={this.props.students}/>
+            </Collapse>
           </div>
         </div>
     );
