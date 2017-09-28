e04709cee99dddc15998ae033b74cfed2abe13a4
Marcelo Conilho <marcelo.conilho@hotmail.com>
2017-09-28 17:40:12 +0000

    Changing /generateStudentCertificate endpoint return

--- backend/src/main/java/com/marcelo/conilho/tg/controller/StudentController.java
+++ backend/src/main/java/com/marcelo/conilho/tg/controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -53,20 +55,16 @@ public class StudentController {
     }
 
     @RequestMapping(value="/generateStudentCertificate", method = RequestMethod.GET)
-    public HttpServletResponse getStudant(HttpServletResponse response, HttpServletRequest request) throws Exception {
+    public ResponseEntity<byte[]> generateStudentCertificate( HttpServletRequest request) throws Exception {
         generateAllStudentsDocument.createPDFwithItext("Marcelo Conilho");
         byte[] contents = Files.readAllBytes(new File("TESTPDF.pdf").toPath());
+       
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/pdf"));
         String filename = "output.pdf";
         headers.setContentDispositionFormData(filename, filename);
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-
-        response.setStatus(200);
-        OutputStream os = response.getOutputStream();
-        os.write(contents);
-        os.flush();
-        os.close();
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
         return response;
     }
 }
